bc35e946902d914b774384215f868ff811fc1f9f
danial pezeshpoor <dpezeshpoor@yahoo.com>
2024-07-17 23:56:34 +0330

    arrays and hashing - ransome note in python

---
+++ Python/arraysAndHashing/ransomeNote.py
@@ -0,0 +1,13 @@
+def can_construct(ransomNote: str, magazine: str) -> bool:
+    alphabetFrequencyTracker = [0] * 26
+    
+    for i in magazine:
+        alphabetFrequencyTracker[ord(i) - 65] += 1
+        
+    for j in ransomNote:
+        alphabetFrequencyTracker[ord(i) - 65] -= 1
+        
+    for k in alphabetFrequencyTracker:
+        if k < 0: return False
+        
+    return True
