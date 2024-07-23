b3edd534afff9e083a3a46bc32d7b5be6ee7ba14
danial pezeshpoor <dpezeshpoor@yahoo.com>
2024-07-23 16:42:21 +0330

    file and directories name are improved

--- Go/main.go
+++ Go/main.go
@@ -1,12 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/daniial79/LeetCode-Problems/twoPointers"
-)
+import "fmt"
 
 func main() {
-	fmt.Println(twoPointers.TwoSum([]int{0, 0, 3, 4}, 0))
-	fmt.Println(twoPointers.TwoSum([]int{2, 7, 11, 15}, 9))
+	fmt.Println("Hello There!")
 }
